953a9acecb71f0c4d25879a3741c9ed856490f03
angkiki <gabrielangzw@gmail.com>
2018-07-24 15:46:40 +0800

    added nested attributes to request and validation to items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
   belongs_to :request
+  validates_presence_of :name, :description, :qty
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,5 @@
 class Request < ApplicationRecord
   validates_presence_of :title
   has_many :items
+  accepts_nested_attributes_for :items, allow_destroy: true
 end
