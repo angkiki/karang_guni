5a332e3b612230a8f996b9dee04f17c73ada4f4a
angkiki <gabrielangzw@gmail.com>
2018-07-25 19:05:48 +0800

    amended request routes, added association to sellers

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,6 @@
 class Request < ApplicationRecord
+  belongs_to :seller
+
   validates_presence_of :title
   has_many :items, inverse_of: :request, dependent: :destroy
   accepts_nested_attributes_for :items, allow_destroy: true
