017d8ddbadc3663a259c5266491cfd66ecf4c96d
Phangster <mynamebryanphang@gmail.com>
2018-07-30 13:50:56 +0800

    remove migration error

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2018_07_29_233458) do
-=======
-ActiveRecord::Schema.define(version: 2018_07_25_154339) do
->>>>>>> 43fa1d3ceebefa685c20d1c0fe74a3b4cb7a7868
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,13 +43,6 @@ ActiveRecord::Schema.define(version: 2018_07_25_154339) do
     t.string "avatar"
     t.index ["email"], name: "index_buyers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_buyers_on_reset_password_token", unique: true
-  end
-
-  create_table "conversations", force: :cascade do |t|
-    t.integer "sender_id"
-    t.integer "recipient_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "items", force: :cascade do |t|
@@ -107,14 +96,6 @@ ActiveRecord::Schema.define(version: 2018_07_25_154339) do
     t.string "avatar"
     t.index ["email"], name: "index_sellers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_sellers_on_reset_password_token", unique: true
-  end
-
-  create_table "sender_reciever", force: :cascade do |t|
-    t.integer "sender_id"
-    t.integer "reciever_id"
-    t.text "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "items", "requests"
